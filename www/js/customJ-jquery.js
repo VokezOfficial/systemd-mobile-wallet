c54e4632d0fd28ce44747ae5c5fbe1678cc6c8cd
bhanuwhite <feather.white@gmail.com>
2019-08-13 23:46:39 +0530

    updated styles for #12,#17,#18,#19,#20,#21,#23,#25,#28,#31,#36,#37,#42,#45

---
+++ www/js/customJ-jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    if( $('#tablogin').css('display') == 'none' ) {
+       console.log('It equal block');
+    } else {
+       console.log('It did not equal block');
+    }
+});
+
