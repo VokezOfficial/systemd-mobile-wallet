0a3874b9359ba9cc840cf7c0fa434ea47179f827
bhanuwhite <feather.white@gmail.com>
2019-08-23 19:08:43 +0530

    did some changes  21,22,24,39,46 and changed 4 pop

--- www/js/customJ-jquery.js
+++ www/js/customJ-jquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     
-    if( $('#tablogin').css('display') == 'none' ) {
+    if( $('#lblaccsecret').css('display') == 'none' ) {
        console.log('It equal block');
     } else {
        console.log('It did not equal block');
